29bf2ebf489ebde0e3db3704a8ea0c28c8a9adf3
Laetitia Aillaud <laetitia.aillaud@hotmail.fr>
2021-06-11 15:38:46 +0200

    Contenu page d'accueil et liens vers les pages produits

---
+++ js/index.js
@@ -0,0 +1,36 @@
+main() // Main function, dès le chargement de la page 
+
+async function main() {
+    const products = await getProducts()
+
+    for (product of products) { //Boucle pour afficher autant de produits qu'il y en a dans l'API
+        displayProduct(product) //Affichage des produits 
+    }
+}
+
+// Fonction pour récupérer les produitss : 
+function getProducts() {
+    return fetch("http://localhost:3000/api/cameras")
+        .then(function(httpBodyResponse) {
+            return httpBodyResponse.json()
+        })
+        .then(function(products){
+            return products
+        })
+        .catch(function(error) {
+            alert(error)
+        })      
+}
+
+// Fonction pour afficher les infos des produits : 
+function displayProduct(product) {
+    const templateElt = document.getElementById("templateProduct")
+    const cloneElt = document.importNode(templateElt.content, true) 
+
+    cloneElt.getElementById("card__link").href = `pages/products.html?id=${product._id}` //URL de la fiche produit correspondante 
+    cloneElt.getElementById("card__img").src = product.imageUrl //Image du produit
+    cloneElt.getElementById("card__name").textContent = product.name //Nom du produit 
+    cloneElt.getElementById("card__price").textContent = product.price/100 + ".00€" //Prix du produit
+
+    document.getElementById("productList").appendChild(cloneElt)
+}
