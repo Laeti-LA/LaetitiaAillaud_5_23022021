0ab8a07361af78c8408ae7410ff9c1c4f881f43c
Laetitia Aillaud <laetitia.aillaud@hotmail.fr>
2021-06-14 18:00:48 +0200

    Page produit : récupération ID dans l'URL

--- js/product.js
+++ js/product.js
@@ -1,3 +1,20 @@
 // Récupérer ID produit dans l'URL 
 let params = (new URL(document.location)).searchParams;
-let productId = params.get('id');
+let productId = params.get('id'); 
+
+
+// Fonction pour récupérer les données des produits : 
+function getProductInfo() {
+    return fetch(`http://localhost:3000/api/cameras/${productId}`)
+        .then(function(httpBodyResponse) {
+            return httpBodyResponse.json();
+        })
+        .then((productInfo) => productInfo)
+}
+
+function displayProductInfo() {
+    document.getElementById("product__id") = productId //Id du produit
+    document.getElementById("product__img").src = product.imageUrl //Image du produit
+    document.getElementById("product__name").textContent = product.name //Nom du produit 
+    document.getElementById("product__price").textContent = product.price/100 + ".00€" //Prix du produit
+}
