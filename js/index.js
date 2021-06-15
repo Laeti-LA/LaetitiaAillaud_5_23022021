a5c46fd7d70122a24aef5e063cca269ff78a4b78
Laetitia Aillaud <laetitia.aillaud@hotmail.fr>
2021-06-15 11:55:21 +0200

    Page produit : affichage des données du produit (paramètres de requête de l'URL)

--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
 main() // Main function, dès le chargement de la page 
-
 async function main() {
-    const products = await getProducts()
+    const products = await getProducts() //Récupération des produits 
 
     for (product of products) { //Boucle pour afficher autant de produits qu'il y en a dans l'API
         displayProduct(product) //Affichage des produits 
     }
 }
 
-// Fonction pour récupérer les produitss : 
+// Fonction pour récupérer les produits : 
 function getProducts() {
     return fetch("http://localhost:3000/api/cameras")
         .then(function(httpBodyResponse) {
@@ -27,9 +26,13 @@ function displayProduct(product) {
     const templateElt = document.getElementById("templateProduct")
     const cloneElt = document.importNode(templateElt.content, true) 
 
-    cloneElt.getElementById("card__link").href = `pages/products.html?id=${product._id}` //URL de la fiche produit correspondante 
-    cloneElt.getElementById("card__img").src = product.imageUrl //Image du produit
-    cloneElt.getElementById("card__name").textContent = product.name //Nom du produit 
+    //URL de la fiche produit correspondante (doit contenir l'id après un "?" pour récupérer et afficher les infos dans la page produit)
+    cloneElt.getElementById("card__link").href = `pages/products.html?id=${product._id}` 
+    //Adresse de l'image du produit
+    cloneElt.getElementById("card__img").src = product.imageUrl 
+    //Nom du produit
+    cloneElt.getElementById("card__name").textContent = product.name 
+    //Prix du produit 
     cloneElt.getElementById("card__price").textContent = product.price/100 + ".00€" //Prix du produit
 
     document.getElementById("productList").appendChild(cloneElt)
--- js/product.js
+++ js/product.js
@@ -1,20 +1,40 @@
-// Récupérer ID produit dans l'URL 
-let params = (new URL(document.location)).searchParams;
-let productId = params.get('id'); 
-
-
-// Fonction pour récupérer les données des produits : 
-function getProductInfo() {
-    return fetch(`http://localhost:3000/api/cameras/${productId}`)
-        .then(function(httpBodyResponse) {
-            return httpBodyResponse.json();
-        })
-        .then((productInfo) => productInfo)
+main() // Main function, dès le chargement de la page 
+async function main() {
+    const productId = getProductId()
+    const productData = await getProductData(productId)
+    displayProductInfo(productData);
 }
 
-function displayProductInfo() {
-    document.getElementById("product__id") = productId //Id du produit
-    document.getElementById("product__img").src = product.imageUrl //Image du produit
-    document.getElementById("product__name").textContent = product.name //Nom du produit 
-    document.getElementById("product__price").textContent = product.price/100 + ".00€" //Prix du produit
+// Fonction pour récupérer l'id du produit dans l'url 
+function getProductId() {
+    return new URL(window.location.href).searchParams.get('id');
+}
+// Fonction pour récupérer les données des produits 
+function getProductData(productId) {
+    return fetch(`http://localhost:3000/api/cameras/${productId}`)
+    .then(function(httpBodyResponse) {
+        return httpBodyResponse.json()
+    })
+    .then(function(productData){
+        return productData
+    })
+    .catch(function(error) {
+        alert(error)
+    })  
+}
+
+// Fonction pour afficher les infos du produit 
+function displayProductInfo(productData) {
+    //Titre de la page (Modèle + Nom du produit)
+    document.getElementById("product__title").textContent = "Modèle " + productData.name 
+    //Référence du produit
+    document.getElementById("product__id").textContent = "Réf. : " + productData._id 
+    //Adresse de l'image du produit
+    document.getElementById("product__img").src = productData.imageUrl 
+    //Nom du produit
+    document.getElementById("product__name").textContent = productData.name 
+    //Prix du produit (à transformer en nombre/devise!!!)
+    document.getElementById("product__price").textContent = productData.price/100 + ".00€" 
+    //Description du produit
+    document.getElementById("product__description").textContent = productData.description 
 }
