c5d9191d38edec915b0c9b8d1c972f0d6950c434
Laetitia Aillaud <laetitia.aillaud@hotmail.fr>
2021-06-17 18:58:09 +0200

    Ajout eventListener sur le bouton d'ajout au panier et création d'un objet contenant les infos du produit en question (pour localStorage)

--- js/product.js
+++ js/product.js
@@ -1,10 +1,13 @@
 main() // Main function, dès le chargement de la page 
 async function main() {
     const productId = getProductId(); //Etape 1 : récupérer id du produit grâce à son url
-    const productData = await getProductData(productId); //Etape 2 : récupérer info produit grâce à son id
+    const productData = await getProductData(productId); //Etape 2 : récupérer les données du produit grâce à son id
     displayProductInfo(productData); //Etape 3 : afficher les infos du produit 
     displayProductOptions(productData); //Etape 4 : afficher les options de personnalisation du produit
+
 }
+
+// _______________________ AFFICHAGE DE LA PAGE PRODUIT _______________________
 
 // Fonction pour récupérer l'id du produit dans l'url 
 function getProductId() {
@@ -24,7 +27,7 @@ function getProductData(productId) {
     })  
 }
 
-// Fonction pour afficher les infos du produit 
+// Fonction pour afficher les infos du produit sur la page et dans un objet
 function displayProductInfo(productData) {
     //Titre de la page (Modèle + Nom du produit)
     document.getElementById("product__title").textContent = "Modèle " + productData.name;
@@ -34,17 +37,47 @@ function displayProductInfo(productData) {
     document.getElementById("product__img").src = productData.imageUrl;
     //Nom du produit
     document.getElementById("product__name").textContent = productData.name; 
-    //Prix du produit (à transformer en nombre/devise!!!)
+    //Prix du produit 
     document.getElementById("product__price").textContent = productData.price/100 + ".00€";
     //Description du produit
     document.getElementById("product__description").textContent = productData.description;
 
+    //_________________________ AJOUT PRODUITS AU PANIER __________________________ 
+    // Event listener du bouton "Ajouter au panier"
+    const btnAddToCart = document.getElementById("btn_addToCart"); 
+    btnAddToCart.addEventListener("click", (event) => {
+        event.preventDefault(); // Ne pas actualiser la page lors du clic 
+
+        // Création d'un objet contenant les infos du produit ajouté au panier
+        let productToAdd = {
+            product_name: productData.name,
+            product_id: productData._id,
+            product_price: productData.price / 100,
+        }
+        
+        console.log(productToAdd); 
+        let cartProduct = JSON.parse(localStorage.getItem("cart"));
+      
+      
+
+})
+
 }
 
-// Récupérer options de personnalisation 
+// Fonction pour récupérer et afficher les options de personnalisation 
 function displayProductOptions(productData) {
     productData.lenses.forEach(lens => {
         let productOption = document.createElement("option");
         document.getElementById("product__option--selector").appendChild(productOption).innerHTML = lens;
     });
 }
+
+
+
+
+
+
+
+
+
+
